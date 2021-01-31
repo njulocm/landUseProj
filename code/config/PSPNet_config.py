6c6a17ba73d831340c3b3e67584ea32d988367cc
cm <njulocm@163.com>
2021-01-31 19:19:31 +0800

    update

--- code/config/PSPNet_config.py
+++ code/config/PSPNet_config.py
@@ -1,8 +1,10 @@
 from torchvision import transforms as T
+from torch import nn
 
 num_classes = 10
-device = 'cuda:2'
+device = 'cuda:3'
 root_dir = '/home/cm/landUseProj'
+logfile = root_dir+'/code/log/PSPNet_aus-log-0131.log'
 
 train_mean = [0.12115830639715953, 0.13374122921202505, 0.10591787170772765, 0.5273172240088813]
 train_std = [0.06708223199001057, 0.07782029730399954, 0.06915748947925031, 0.16104953671241798]
@@ -10,7 +12,9 @@ test_mean = [0.1070993648354524, 0.10668084722780714, 0.10053905204822813, 0.403
 test_std = [0.0659528024287434, 0.07412065904513164, 0.07394464607772513, 0.1716164042414669]
 
 dataset_cfg = dict(
-    train_dir=root_dir + '/tcdata/suichang_round1_train_210120',
+    train_dir = root_dir + '/tcdata/train',
+    # train_dir=root_dir + '/tcdata/argument',
+    val_dir=root_dir + '/tcdata/validation',
     test_dir=root_dir + '/tcdata/suichang_round1_test_partA_210120',
     input_channel=4,  # 使用几个通道作为输入
     train_ratio=0.8,
@@ -26,12 +30,19 @@ dataset_cfg = dict(
 )
 
 model_cfg = dict(
-    type='CheckPoint', # 由于系统更新中断了，从epoch9的结果开始重新训练
+    type='PSPNet',
+    aux_weight = 0.4, # 辅助loss权重
+    layers=50, # resnet的层数
     input_channel=dataset_cfg['input_channel'],
     num_classes=num_classes,
     pretrained=False,
-    use_aux=False,
-    check_point_file=root_dir + '/code/checkpoint/PSPNet/PSPNet_model.pth',
+
+    bins=(1, 2, 3, 6),
+    dropout=0.1,
+    zoom_factor=8,
+    use_ppm=True,
+
+    check_point_file=root_dir + '/code/checkpoint/PSPNet_aux/PSPNet_aux_model.pth',
     device=device, # 加载checkPoint需要指定device，否则会默认加载到cuda:0而显存溢出
 
 )
@@ -40,7 +51,9 @@ train_cfg = dict(
     num_workers=4,
     batch_size=16,
     num_epochs=100,
-    optimizer_cfg=dict(type="adam", lr=0.01)
+    # optimizer_cfg=dict(type="adam", lr=0.01),
+    optimizer_cfg=dict(type="sgd", lr=0.01, momentum=0.9, weight_decay=1e-4),
+    is_PSPNet=True
 )
 
 test_cfg = dict(
@@ -49,6 +62,6 @@ test_cfg = dict(
     dataset='test_dataset',
     batch_size=train_cfg['batch_size'],
     num_workers=train_cfg['num_workers'],
-    check_point_file=root_dir + '/code/checkpoint/PSPNet/PSPNet_model-epoch99.pth',
-    out_dir=root_dir + '/prediction_result/PSPNet_test_out-0124',
+    check_point_file=root_dir + '/code/checkpoint/PSPNet_aux/PSPNet_aux_model-epoch99.pth',
+    out_dir=root_dir + '/prediction_result/PSPNet_aux_test_out-0130',
 )
