fbabf57a987007ea68a01e04bc6581eb6df8828a
cm <njulocm@163.com>
2021-03-08 15:03:08 +0800

    update

--- code/model/__init__.py
+++
@@ -1,6 +0,0 @@
-from .build_model import build_model
-from .Unet import U_Net, AttU_Net, NestedUNet
-from .SegNet import SegNet
-from .PSPNet import PSPNet
-from .HRNet import hrnetv2 as HRNet
-from .segmentation_models import SmpNet
--- code/tools/__init__.py
+++
@@ -1,2 +0,0 @@
-from .train import train_main
-from .test import test_main
